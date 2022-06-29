dfc585246d0c4524fdeaea0289e1910ef506c80b
AnzickMedia <87082069+AnzickMedia@users.noreply.github.com>
2022-06-29 12:11:39 -0500

    creacion del componente Quantity, el cual se encargara de administrar la cantidad de productos a comprar

---
+++ src/components/Quantity/Quantity.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './Quantity.scss'
+
+const Quantity = () => {
+
+    const [suma, setSuma] = useState(0)
+
+    const subsQtty =()=>{
+        setSuma(suma-1)
+        if (suma<=0) {
+            setSuma(0)
+        }
+    }
+
+    const addQtty =()=>{
+        setSuma(suma+1)
+        
+    }
+  return (
+    <div className='o-qnt-container'>
+        <div className="o-counter">
+        <p onClick={()=>subsQtty()}>-</p>
+        <p>{suma}</p>
+        <p onClick={()=>addQtty()}>+</p>
+        </div>
+        <div className="o-total-price">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Quantity
