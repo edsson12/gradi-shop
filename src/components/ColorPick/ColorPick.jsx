941b31cfe616e98fb367139df4d72ae340d24a40
AnzickMedia <87082069+AnzickMedia@users.noreply.github.com>
2022-06-29 12:08:21 -0500

    creacion de componente tipo radio para elegir el color del producto

--- src/components/ColorPick/ColorPick.jsx
+++ src/components/ColorPick/ColorPick.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import './ColorPick.scss'
+import React, { useState } from "react";
+import "./ColorPick.scss";
 
-const ColorPick = () => {
+const ColorPick = (resultado,lista) => {
+  
+
+  console.log(lista)
+ 
+  
+  const [color, setColor] = useState(1);
+  const changeRadio = (e) => {
+    setColor(e.target.value);
+  };
   return (
-    <div>ColorPick</div>
-  )
-}
+    <div className="o-cp-container">
+      <p>Colors:</p>
+      <label className="custom-radio-btn">
+        <input
+          type="radio"
+          id="radio1"
+          value={1}
+          checked={color == 1 ? true : false}
+          onChange={changeRadio}
+        />
+        <span className="checkmark"></span>
+      </label>
 
-export default ColorPick
+      <label className="custom-radio-btn">
+        <input
+          type="radio"
+          id="radio2"
+          value={2}
+          checked={color == 2 ? true : false}
+          onChange={changeRadio}
+        />
+        <span className="checkmark"></span>
+      </label>
+      <p>{color}</p>
+      
+      
+    </div>
+  );
+};
+
+export default ColorPick;
